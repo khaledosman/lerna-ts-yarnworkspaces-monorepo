f3dea55a1411004b5a9612ad50f4d27021c1d72d
Khaled Osman <khaled.osmaan@gmail.com>
2020-06-17 14:03:01 +0200

    delete gitignored files

--- packages/serviceB/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const serviceA_1 = require("@colorfy/serviceA");
-console.log(serviceA_1.sum(1, 2));
-//# sourceMappingURL=index.js.map
